e1de20ae2b67b03c56e8b85ff14884690c37e5cd
KB <roachhd@users.noreply.github.com>
2014-10-15 23:42:54 +1100

    Soon, really 🙊

--- script.js
+++ script.js
@@ -327,7 +327,7 @@
         $restart.css({width: 100, height: 40, top: '50%', left: '50%', marginLeft: -50, marginTop: -20, letterSpacing: '1px', 
                       background: Colors.white, color: Colors.blue, fontSize: 22, textAlign: 'center', lineHeight: '40px',
                       cursor: 'pointer', opacity: 0});
-        $restart.html('RESTART');
+        $restart.html('COMING SOON');
         $restart.on('click', restart);
         $restart.hide();
 
